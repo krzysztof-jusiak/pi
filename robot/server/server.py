87098fa7a60243518db6170da411d3dd9009b09c
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-09-02 20:32:16 +0100

    fix

--- robot/server/server.py
+++ robot/server/server.py
@@ -119,8 +119,8 @@ class HTTPHandler(BaseHTTPRequestHandler):
           self.wfile.close()
 
         elif self.path.startswith("/forward"):
-          HTTPHandler.left = max(0, int(self.path.split(':')[1]) - 15)
-          HTTPHandler.right = max(0, int(self.path.split(':')[2]))
+          HTTPHandler.left = max(0, int(self.path.split(':')[1]))
+          HTTPHandler.right = max(0, int(self.path.split(':')[2]) - 15)
           print "forward: " + str(HTTPHandler.left) + ":" + str(HTTPHandler.right)
 
           #engine left
