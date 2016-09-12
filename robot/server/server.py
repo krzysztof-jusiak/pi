1dc79565d6cbacec811808ad8c36cec8618ebdf6
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-09-12 20:56:19 +0100

    fi

--- robot/server/server.py
+++ robot/server/server.py
@@ -62,10 +62,12 @@ def sonar_distance(trig_pin = SONAR_TRIGGER, echo_pin = SONAR_ECHO, sample_size 
       GPIO.output(trig_pin, True)
       time.sleep(0.00001)
       GPIO.output(trig_pin, False)
-      while GPIO.input(echo_pin) == 0:
-          sonar_signal_off = time.time()
-      while GPIO.input(echo_pin) == 1:
-          sonar_signal_on = time.time()
+      timeout = time.time() + 0.5
+      while GPIO.input(echo_pin) == GPIO.LOW and time.time() < timeout: pass
+        sonar_signal_off = time.time()
+      timeout = time.time() + 0.5
+      while GPIO.input(echo_pin) == GPIO.HIGH and time.time() < timeout: pass
+        sonar_signal_on = time.time()
       time_passed = sonar_signal_on - sonar_signal_off
       distance_cm = time_passed * ((speed_of_sound * 100) / 2)
       sample.append(distance_cm)
