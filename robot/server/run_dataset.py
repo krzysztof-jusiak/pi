b8ba0b925dd0085a46d8ffda9b90933ab70f0b0e
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-10-08 21:51:27 +0100

    degrees

--- robot/server/run_dataset.py
+++ robot/server/run_dataset.py
@@ -29,12 +29,13 @@ while(cv2.waitKey(10) != ord('q')):
   right = int(file.split('_')[3].split('.')[0])
   image = scipy.misc.imresize(scipy.misc.imread("data/" + file)[-150:], [66, 200]) / 255.0
   original = float((left) - (right)) * 4.0 
-  degrees = 4.0 * (model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / scipy.pi + 15.0)
+  degrees = 4.0 * (model.y.eval(feed_dict={model.x: [image], model.keep_prob: 1.0})[0][0] * 180.0 / scipy.pi) - 15.0
   print("Predicted steering angle: " + str(degrees) + ", " + str(original))
   cv2.imshow("frame", image)
   #make smooth angle transitions by turning the steering wheel based on the difference of the current angle
   #and the predicted angle
-  smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
+  smoothed_angle = degrees
+#smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
   M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
   dst = cv2.warpAffine(img,M,(cols,rows))
   cv2.imshow("steering wheel", dst)
